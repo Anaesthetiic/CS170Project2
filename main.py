fcf21aab01c005694f32d492f7357db10ea7b8d0
Anaesthetiic <tyoniclebionicle@gmail.com>
2024-11-20 10:17:04 -0800

    Start forward_selection

--- main.py
+++ main.py
@@ -2,17 +2,41 @@ import random
 
 # Not sure if we'll need this
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        self.data = []
         self.parent = None
         self.children = []
+        self.indexBestFeature = -1
+        self.highestAccuracy = -1  # float [0..100] representing percentage
 
     def add_child(self, child_node):
         child_node.parent = self
         self.children.append(child_node)
+        
+    def add_data(self, evalFunc):
+        self.data.append(evalFunc)
+        
+    def set_index_best_feature(self, index):
+        self.indexBestFeature = index
+
+    def get_index_best_feature(self):
+        return self.indexBestFeature
+    
+    def set_highest_accuracy(self, accuracy):
+        self.highestAccuracy = accuracy
+        
+    def get_highest_accuracy(self):
+        return self.highestAccuracy
 
 def forward_selection(n):
-    pass
+    highestAccuracyPtr = None
+    currNode = None
+    firstIt = True
+    while firstIt or (currNode.get_highest_accuracy() > highestAccuracyPtr.get_highest_accuracy()):
+        firstIt = False
+        node = Node()
+        for i in range(n):      # for n features
+            node.add_data(evalFunc())   # feature i (indexed) has a eval of ___
 
 
 def backward_elimination(n):
@@ -27,6 +51,7 @@ def main():
     # print("3. Bertie’s Special Algorithm.")
     choice = input("Option: ")
     if choice == '1':
+        print("Beginning search.\n")
         forward_selection(total_features)
     elif choice == '2':
         backward_elimination(total_features)
@@ -38,6 +63,7 @@ def main():
 if __name__ == "__main__":
     main()
     
-# EVAL FUNCTION STUB, RETURNS RANDOM VAL
-def evalFunc(upperbound=255):
-    return random.randint(0, upperbound)
+# EVAL FUNCTION STUB, RETURNS RANDOM %VAL
+def evalFunc(upperbound=100):
+    # return random.randint(0, upperbound)
+    return random.uniform(0, upperbound)
